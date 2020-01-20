a6dbc0231f8c3c696736356be2f22079b4f122b8
Kacper Szmigiel <szmigielkacper@gmai.com>
2020-01-20 14:45:41 +0100

    all kinds of buttons

---
+++ lib_lego/lib/dropdown_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class DropdownButtonWidget extends StatefulWidget {
+  DropdownButtonWidget({Key key}) : super(key: key);
+
+  @override
+  _DropdownButtonWidgetState createState() => _DropdownButtonWidgetState();
+}
+
+class _DropdownButtonWidgetState extends State<DropdownButtonWidget> {
+  String dropdownValue = 'One';
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: Icon(Icons.arrow_downward),
+      iconSize: 24,
+      elevation: 16,
+      underline: Container(
+        height: 2,
+      ),
+      onChanged: (String newValue) {
+        setState(() {
+          dropdownValue = newValue;
+        });
+      },
+      items: <String>['One', 'Two', 'Three', 'Four']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+}
