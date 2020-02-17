1cb55fc58dd14c715ebf3ff7bb0c7d29a852b761
Kacper Szmigiel <szmigielkacper@gmai.com>
2020-02-17 19:10:22 +0100

    basic styled onboarding, images not working yet

--- app_motim_fit/lib/ui/onboarding.dart
+++ app_motim_fit/lib/ui/onboarding.dart
@@ -24,10 +24,10 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     return AnimatedContainer(
       duration: Duration(milliseconds: 150),
       margin: EdgeInsets.symmetric(horizontal: 8.0),
-      height: 8.0,
-      width: isActive ? 24.0 : 16.0,
+      height: 10.0,
+      width: 10.0,
       decoration: BoxDecoration(
-        color: isActive ? Colors.white : Color(0xFF7B51D3),
+        color: isActive ? Colors.white : Color(0xFFC4C4C4),
         borderRadius: BorderRadius.all(Radius.circular(12)),
       ),
     );
@@ -40,36 +40,13 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
         value: SystemUiOverlayStyle.light,
         child: Container(
           decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              stops: [0.1, 0.4, 0.7, 0.9],
-              colors: [
-                Color(0xFF3594DD),
-                Color(0xFF4563DB),
-                Color(0xFF5036D5),
-                Color(0xFF5B16D0),
-              ],
-            ),
+            color: Color(0xFF242424),
           ),
           child: Padding(
             padding: EdgeInsets.symmetric(vertical: 40.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Container(
-                  alignment: Alignment.centerRight,
-                  child: FlatButton(
-                    onPressed: () => print('Skip'),
-                    child: Text(
-                      'Skip',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20.0,
-                      ),
-                    ),
-                  ),
-                ),
                 Container(
                   height: 600.0,
                   child: PageView(
@@ -97,14 +74,14 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                             ),
                             SizedBox(height: 30.0),
                             Text(
-                              'Connect people\naround the world',
+                              'Be active with',
                               style: kTitleStyle,
                             ),
-                            SizedBox(height: 15.0),
                             Text(
-                              'Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
-                              style: kSubtitleStyle,
+                              'Motim Fit!',
+                              style: kTitleStyleBold,
                             ),
+                            SizedBox(height: 15.0),
                           ],
                         ),
                       ),
@@ -123,14 +100,14 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                               ),
                             ),
                             SizedBox(height: 30.0),
-                            Text(
-                              'Live your life smarter\nwith us!',
-                              style: kTitleStyle,
-                            ),
-                            SizedBox(height: 15.0),
-                            Text(
-                              'Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
-                              style: kSubtitleStyle,
+                            RichText(
+                              text: TextSpan(
+                                style: kTitleStyle,
+                                children: <TextSpan>[
+                                  TextSpan(text: 'Get out of\nthe house and\ncollect '),
+                                  TextSpan(text: 'coupons.', style: kTitleStyleBold),
+                                ],
+                              ),
                             ),
                           ],
                         ),
@@ -150,14 +127,14 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                               ),
                             ),
                             SizedBox(height: 30.0),
-                            Text(
-                              'Get a new experience\nof imagination',
-                              style: kTitleStyle,
-                            ),
-                            SizedBox(height: 15.0),
-                            Text(
-                              'Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tempor incididunt ut labore et.',
-                              style: kSubtitleStyle,
+                            RichText(
+                              text: TextSpan(
+                                style: kTitleStyle,
+                                children: <TextSpan>[
+                                  TextSpan(text: 'Motim Fit\nfor '),
+                                  TextSpan(text: 'everyone!', style: kTitleStyleBold),
+                                ],
+                              ),
                             ),
                           ],
                         ),
@@ -169,68 +146,29 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: _buildPageIndicator(),
                 ),
-                _currentPage != _numPages - 1
-                    ? Expanded(
-                  child: Align(
-                    alignment: FractionalOffset.bottomRight,
-                    child: FlatButton(
-                      onPressed: () {
-                        _pageController.nextPage(
-                          duration: Duration(milliseconds: 500),
-                          curve: Curves.ease,
-                        );
-                      },
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Text(
-                            'Next',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 22.0,
-                            ),
-                          ),
-                          SizedBox(width: 10.0),
-                          Icon(
-                            Icons.arrow_forward,
-                            color: Colors.white,
-                            size: 30.0,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                )
-                    : Text(''),
               ],
             ),
           ),
         ),
       ),
-      bottomSheet: _currentPage == _numPages - 1
-          ? Container(
-        height: 100.0,
+      bottomSheet: Container(
+        height: 68.0,
         width: double.infinity,
-        color: Colors.white,
+        color: Color(0xFF242424),
         child: GestureDetector(
-          onTap: () => print('Get started'),
-          child: Center(
-            child: Padding(
-              padding: EdgeInsets.only(bottom: 30.0),
-              child: Text(
-                'Get started',
-                style: TextStyle(
-                  color: Color(0xFF5B16D0),
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                ),
+          onTap: () => Navigator.pushNamed(context, '/splash'),
+          child: Padding(
+            padding: EdgeInsets.only(top: 17.0, left: 74.0),
+            child: Text(
+              'Get started',
+              style: TextStyle(
+                color: Color(0xFFDED5BA),
+                fontSize: 20.0,
               ),
             ),
           ),
         ),
-      )
-          : Text(''),
+      ),
     );
   }
 }
--- app_motim_fit/lib/utilities/styles.dart
+++ app_motim_fit/lib/utilities/styles.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 final kTitleStyle = TextStyle(
   color: Colors.white,
   fontFamily: 'CM Sans Serif',
-  fontSize: 26.0,
-  height: 1.5,
+  fontSize: 40.0,
+  height: 1.25,
 );
 
-final kSubtitleStyle = TextStyle(
+final kTitleStyleBold = TextStyle(
   color: Colors.white,
-  fontSize: 18.0,
-  height: 1.2,
+  fontFamily: 'CM Sans Serif',
+  fontSize: 40.0,
+  height: 1.25,
+  fontWeight: FontWeight.bold,
 );
