de65f7585863a217339622dfac25fb6287e7ceaf
Peter <dante383@protonmail.com>
2020-03-06 23:21:06 +0100

    simplify code

---
+++ app_kodesmil_legobook/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/somewolf/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/media/somewolf/2279019114589267/Projects/KodeSmil/kodesmil-mobile/app_kodesmil_legobook"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/somewolf/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
